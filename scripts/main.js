b90d6dc3e0821d33a1eb7bbd91296058f1019098
midnightnymph <skn.markham@gmail.com>
2024-07-24 11:14:33 -0400

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -121,7 +121,7 @@ function keywordEncrypt(inputText) {
     let encoded = inputText.toUpperCase().split('').map(char => {
         if (alphabet.includes(char)) {
             return cipherAlphabet[alphabet.indexOf(char)];
-        }
+        } 
         return char;
     }).join('');
     console.log(inputText, keyword, encoded);
